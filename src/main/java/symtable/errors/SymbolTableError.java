9947bd9a55207251147d011034bedaf52784be57
luserazo <serazo.luis@gmail.com>
2018-03-23 05:13:07 -0400

    first symbol table commit

---
+++ src/main/java/symtable/errors/SymbolTableError.java
@@ -0,0 +1,16 @@
+/*
+ * File: SymbolTableError.java
+ *
+ * Desc: contains the errors associated with symbol
+ *       table routines.
+ */
+package main.java.symtable.errors;
+
+/**
+ * SymbolTableError
+ * @author Luis Serazo
+ */
+
+public class SymbolTableError {
+
+}
